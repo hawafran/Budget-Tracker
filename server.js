08556d8a1f77e7783cfda4ef27d76d13ac781d3b
Hawa <hawa.francis@gmail.com>
2021-06-28 12:50:18 -0700

    commit

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+// const express = require("express");
+// const logger = require("morgan");
+// const mongoose = require("mongoose");
+// const compression = require("compression");
+// const apiRoutes = require("./routes/api.js");
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+
+// app.use(logger("dev"));
+
+// app.use(compression());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("public"));
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/Hawa-budget-tracker',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+// );
+
+// // routes
+// app.use(apiRoutes);
+
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -16,17 +51,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Hawa-budget-tracker',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/Hawa-budget-tracker";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
-// routes
 app.use(apiRoutes);
 
 app.listen(PORT, () => {
